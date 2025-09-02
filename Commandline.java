7a1ce7c5111fc7eaac3e797ddc71008ddd263dbf
CosmicCoder07 <abhiyadav4612@gmail.com>
2025-09-02 07:31:40 +0530

    idk

---
+++ Commandline.java
@@ -0,0 +1,9 @@
+class Commandline {
+    static public void main(String[] args){
+        System.out.println("Number of arguments: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+    }
+}
+
