f1b61b9b054f577e2c7b4597e6a3a41a644603f8
CosmicCoder07 <abhiyadav4612@gmail.com>
2025-09-09 09:23:13 +0530

    new update

---
+++ lecture/TwoDarrray.java
@@ -0,0 +1,22 @@
+package lecture;
+
+public class TwoDarrray {
+    public static void main(String[] args) {
+        
+        int[][] m = {
+            {10, 20, 30},
+            {40, 50, 60},
+            {70, 80, 90}
+        };
+
+        
+        System.out.println("Matrix values:");
+        for (int i = 0; i < m.length; i++) {          
+            for (int j = 0; j < m[i].length; j++) {   
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();  
+        }
+    }
+}
+
