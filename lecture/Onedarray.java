aafea2aaf9f7639dec961d3c045c7e7978816303
CosmicCoder07 <abhiyadav4612@gmail.com>
2025-09-09 08:58:03 +0530

    Update

--- lecture/Onedarray.java
+++ lecture/Onedarray.java
@@ -1,30 +1,27 @@
 package lecture;
 
-public class Lec3 {
-    
+public class Onedarray {
     public static void main(String[] args) {
-        // Step 1: Create an array of size 5
+        
         int[] marks = new int[5];
 
-        // Step 2: Assign values
+   
         marks[0] = 90;
         marks[1] = 85;
         marks[2] = 70;
         marks[3] = 95;
         marks[4] = 88;
 
-        // Step 3: Print values using a loop
+        
         System.out.println("Marks of students:");
         for (int i = 0; i < marks.length; i++) {
             System.out.println("Student " + (i + 1) + ": " + marks[i]);
         }
 
-        // Step 4: Using enhanced for loop (shorter way)
+    
         System.out.println("\nPrinting again using enhanced for loop:");
         for (int mark : marks) {
             System.out.println(mark);
         }
     }
 }
-
-
