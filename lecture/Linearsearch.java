731c5be533548d40bfce6446bf0f6fc344ae7699
CosmicCoder06 <k.avhi4612@gmail.com>
2025-09-11 14:05:38 +0530

    new update

--- lecture/Linearsearch.java
+++ lecture/Linearsearch.java
@@ -2,20 +2,20 @@ package lecture;
 
 public class Linearsearch {
 
-    // Method for linear search
+    
     public static int linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
-                return i; // return index if found
+                return i; 
             }
         }
-        return -1; // return -1 if not found
+        return -1; 
     }
 
     public static void main(String[] args) {
         int[] marks = {90, 85, 70, 95, 88};
 
-        int key = 95; // the number we want to search
+        int key = 95; 
         int result = linearSearch(marks, key);
 
         if (result == -1) {
