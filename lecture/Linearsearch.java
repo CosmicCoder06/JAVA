26ee5a0874dbf14e7480737ad6426d3c6139e1e6
CosmicCoder06 <k.avhi4612@gmail.com>
2025-11-13 14:10:40 +0530

    new java code

--- lecture/Linearsearch.java
+++ lecture/Linearsearch.java
@@ -1,27 +1,27 @@
-package lecture;
+// package lecture;
 
-public class Linearsearch {
+// public class Linearsearch {
 
     
-    public static int linearSearch(int[] arr, int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key) {
-                return i; 
-            }
-        }
-        return -1; 
-    }
+//     public static int linearSearch(int[] arr, int key) {
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] == key) {
+//                 return i; 
+//             }
+//         }
+//         return -1; 
+//     }
 
-    public static void main(String[] args) {
-        int[] marks = {90, 85, 70, 95, 88};
+//     public static void main(String[] args) {
+//         int[] marks = {90, 85, 70, 95, 88};
 
-        int key = 95; 
-        int result = linearSearch(marks, key);
+//         int key = 95; 
+//         int result = linearSearch(marks, key);
 
-        if (result == -1) {
-            System.out.println("Element not found!");
-        } else {
-            System.out.println("Element " + key + " found at index: " + result);
-        }
-    }
-}
+//         if (result == -1) {
+//             System.out.println("Element not found!");
+//         } else {
+//             System.out.println("Element " + key + " found at index: " + result);
+//         }
+//     }
+// }
