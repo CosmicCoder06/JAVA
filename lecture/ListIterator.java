f9adbbf35fc78d1a952d8fe5a4cbb338167b4297
CosmicCoder06 <k.avhi4612@gmail.com>
2025-12-01 11:16:39 +0530

    Day to Day Grind

--- lecture/ListIterator.java
+++ lecture/ListIterator.java
@@ -100,67 +100,150 @@
 //     }
 // }
 
+// import java.util.*;
+
+
+// class Boy implements Comparable<Boy> {
+//     private String name;
+//     private int age;
+//     private String branch;
+
+//     public Boy(String name, int age, String branch){
+//         this.name = name;
+//         this.age = age;
+//         this.branch = branch;
+//     }
+
+
+//     public int compareTo(Boy b){
+//         return this.age-b.getAge();
+//     }
+
+
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public int getAge() {
+//         return age;
+//     }
+
+//     public void setAge(int age) {
+//         this.age = age;
+//     }
+
+//     public String getBranch() {
+//         return branch;
+//     }
+
+//     public void setBranch(String branch) {
+//         this.branch = branch;
+//     }
+// }
+
+// public class comparable {
+//     public static void main(String[] args) {
+//         ArrayList<Boy> list = new ArrayList<>();
+//         list.add(new Boy("Jatin",19,"CS"));
+//         list.add(new Boy("Kanhaiya",18,"CS"));
+//         list.add(new Boy("Ayush",24,"CS"));
+//         list.add(new Boy("Kartik",6,"CS"));
+//         list.add(new Boy("Sanat",13,"CS"));        
+
+
+//         Collections.sort(list);
+
+//         for(Boy b: list){
+//             System.out.println(b.getName()+" "+b.getBranch()+" "+b.getAge());
+//         }
+
+
+
+//     }
+// }
+
 import java.util.*;
-
-
-class Boy implements Comparable<Boy> {
+class Man{
     private String name;
     private int age;
     private String branch;
 
-    public Boy(String name, int age, String branch){
+
+    public Man(String name, int age, String branch){
         this.name = name;
         this.age = age;
         this.branch = branch;
     }
-
-
-    public int compareTo(Boy b){
-        return this.age-b.getAge();
-    }
-
+    //Creating getter
 
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getBranch() {
         return branch;
     }
+}
 
-    public void setBranch(String branch) {
-        this.branch = branch;
+class CompareAge implements Comparator<Man> {
+    public int compare(Man m1, Man m2){
+        return Integer.compare(m1.getAge(),m2.getAge() );
     }
 }
 
-public class comparable {
+class CompareName implements Comparator<Man> {
+    public int compare(Man m1, Man m2){
+        return m1.getName().compareTo(m2.getName());
+    }
+}
+
+class CompareBranch implements Comparator<Man> {
+    public int compare(Man m1, Man m2){
+        return m1.getBranch().compareTo(m2.getBranch());
+    }
+}
+
+
+public class comparator {
     public static void main(String[] args) {
-        ArrayList<Boy> list = new ArrayList<>();
-        list.add(new Boy("Jatin",19,"CS"));
-        list.add(new Boy("Kanhaiya",18,"CS"));
-        list.add(new Boy("Ayush",24,"CS"));
-        list.add(new Boy("Kartik",6,"CS"));
-        list.add(new Boy("Sanat",13,"CS"));        
 
+        ArrayList<Man> list = new ArrayList<>();
+        list.add(new Man("Ayush Sharma",24,"DS"));
+        list.add(new Man("Nukul",20,"CSE"));
+        list.add(new Man("Shivang",18,"CSE"));
+        list.add(new Man("Pari",16,"DS"));
 
-        Collections.sort(list);
+        Collections.sort(list, new CompareAge());
 
-        for(Boy b: list){
-            System.out.println(b.getName()+" "+b.getBranch()+" "+b.getAge());
+        System.out.println("Comparing on the Basis of Age");
+        for(Man m:list){
+            System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
         }
+
+
+        Collections.sort(list, new CompareName());
+        System.out.println("Comparing on the Basis of Name");
+        for(Man m:list){
+            System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
+        }
+
+        Collections.sort(list, new CompareBranch());
+        System.out.println("Comparing on the Basis of Branch");
+        for(Man m:list){
+            System.out.println(m.getName()+" "+m.getAge()+" "+m.getBranch());
+        }
+
+
 
 
 
