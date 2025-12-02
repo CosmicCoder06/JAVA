3e149e32ebd770bfadc596ab18bbf935a4053efc
CosmicCoder06 <k.avhi4612@gmail.com>
2025-12-02 08:56:56 +0530

    Array Revision

---
+++ lecture/Array.java
@@ -0,0 +1,12 @@
+// public class Array {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int[] arr = new int[5];
+//         for(int i = 0; i < arr.length; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i= 0)
+        
+      
+//     }
+// }
--- lecture/ListIterator.java
+++ lecture/ListIterator.java
@@ -1,31 +1,31 @@
-// package Lecture;
+package Lecture;
 
-// import java.util.ArrayList;
-// import java.util.ListIterator;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 
-// public class Listiterator {
-//     public static void main(String[] args) {
-//         ArrayList<Integer> list = new ArrayList<>();
+public class Listiterator {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
 
-//         list.add(10);
-//         list.add(20);
-//         list.add(30);
-//         list.add(40);
-//         list.add(50);
-//         System.out.println(list);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list);
 
-//         ListIterator<Integer> listIterator = list.listIterator(list.size());
+        ListIterator<Integer> listIterator = list.listIterator(list.size());
 
-//         System.out.println("Backward Traversal:");
+        System.out.println("Backward Traversal:");
 
-//         while (listIterator.hasPrevious()) {
-//             System.out.print(listIterator.previous() + " ");
-//         }
+        while (listIterator.hasPrevious()) {
+            System.out.print(listIterator.previous() + " ");
+        }
 
-//         System.out.println();
-//     }
-// }
+        System.out.println();
+    }
+}
 
 // import java.io.File;
 // import java.io.IOException;
@@ -167,85 +167,85 @@
 //     }
 // }
 
-import java.util.*;
-class Man{
-    private String name;
-    private int age;
-    private String branch;
+// import java.util.*;
+// class Man{
+//     private String name;
+//     private int age;
+//     private String branch;
 
 
-    public Man(String name, int age, String branch){
-        this.name = name;
-        this.age = age;
-        this.branch = branch;
-    }
-    //Creating getter
+//     public Man(String name, int age, String branch){
+//         this.name = name;
+//         this.age = age;
+//         this.branch = branch;
+//     }
+//     //Creating getter
 
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public int getAge() {
-        return age;
-    }
+//     public int getAge() {
+//         return age;
+//     }
 
-    public String getBranch() {
-        return branch;
-    }
-}
+//     public String getBranch() {
+//         return branch;
+//     }
+// }
 
-class CompareAge implements Comparator<Man> {
-    public int compare(Man m1, Man m2){
-        return Integer.compare(m1.getAge(),m2.getAge() );
-    }
-}
+// class CompareAge implements Comparator<Man> {
+//     public int compare(Man m1, Man m2){
+//         return Integer.compare(m1.getAge(),m2.getAge() );
+//     }
+// }
 
-class CompareName implements Comparator<Man> {
-    public int compare(Man m1, Man m2){
-        return m1.getName().compareTo(m2.getName());
-    }
-}
+// class CompareName implements Comparator<Man> {
+//     public int compare(Man m1, Man m2){
+//         return m1.getName().compareTo(m2.getName());
+//     }
+// }
 
-class CompareBranch implements Comparator<Man> {
-    public int compare(Man m1, Man m2){
-        return m1.getBranch().compareTo(m2.getBranch());
-    }
-}
-
-
-public class comparator {
-    public static void main(String[] args) {
-
-        ArrayList<Man> list = new ArrayList<>();
-        list.add(new Man("Ayush Sharma",24,"DS"));
-        list.add(new Man("Nukul",20,"CSE"));
-        list.add(new Man("Shivang",18,"CSE"));
-        list.add(new Man("Pari",16,"DS"));
-
-        Collections.sort(list, new CompareAge());
-
-        System.out.println("Comparing on the Basis of Age");
-        for(Man m:list){
-            System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
-        }
+// class CompareBranch implements Comparator<Man> {
+//     public int compare(Man m1, Man m2){
+//         return m1.getBranch().compareTo(m2.getBranch());
+//     }
+// }
 
 
-        Collections.sort(list, new CompareName());
-        System.out.println("Comparing on the Basis of Name");
-        for(Man m:list){
-            System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
-        }
+// public class comparator {
+//     public static void main(String[] args) {
 
-        Collections.sort(list, new CompareBranch());
-        System.out.println("Comparing on the Basis of Branch");
-        for(Man m:list){
-            System.out.println(m.getName()+" "+m.getAge()+" "+m.getBranch());
-        }
+//         ArrayList<Man> list = new ArrayList<>();
+//         list.add(new Man("Ayush Sharma",24,"DS"));
+//         list.add(new Man("Nukul",20,"CSE"));
+//         list.add(new Man("Shivang",18,"CSE"));
+//         list.add(new Man("Pari",16,"DS"));
+
+//         Collections.sort(list, new CompareAge());
+
+//         System.out.println("Comparing on the Basis of Age");
+//         for(Man m:list){
+//             System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
+//         }
 
 
+//         Collections.sort(list, new CompareName());
+//         System.out.println("Comparing on the Basis of Name");
+//         for(Man m:list){
+//             System.out.println(m.getName()+" "+m.getBranch()+" "+m.getAge());
+//         }
+
+//         Collections.sort(list, new CompareBranch());
+//         System.out.println("Comparing on the Basis of Branch");
+//         for(Man m:list){
+//             System.out.println(m.getName()+" "+m.getAge()+" "+m.getBranch());
+//         }
 
 
 
-    }
-}
+
+
+//     }
+// }
